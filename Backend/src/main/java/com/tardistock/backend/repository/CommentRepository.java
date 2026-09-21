a67f65493d06faba8c0ade97ff586b57441d3d95
Huihuing <105479727+Huihuing@users.noreply.github.com>
2026-09-21 17:40:47 +0900

    공개 프로필 댓글 통계 추가 / Add profile comment statistics

--- Backend/src/main/java/com/tardistock/backend/repository/CommentRepository.java
+++ Backend/src/main/java/com/tardistock/backend/repository/CommentRepository.java
@@ -4,9 +4,15 @@ import com.tardistock.backend.entity.Comment;
 import com.tardistock.backend.entity.Member;
 import com.tardistock.backend.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.List;
 
-public interface CommentRepository extends JpaRepository<Comment, Long> {
+public interface CommentRepository
+        extends JpaRepository<Comment, Long> {
+
     List<Comment> findByPostOrderByCreatedAtAsc(Post post);
+
     List<Comment> findByMember(Member member);
-} 
+
+    long countByMember(Member member);
+}
